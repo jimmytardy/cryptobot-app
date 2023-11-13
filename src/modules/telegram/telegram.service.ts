91731725198b7745e8e718aa0821dbd4a7ef6c12
Jimmy <tardyjim26@gmail.com>
2023-11-13 18:50:51 +0100

    change log type

--- src/modules/telegram/telegram.service.ts
+++ src/modules/telegram/telegram.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, Logger } from '@nestjs/common'
 import { OrderBotService } from '../order-bot/order-bot.service'
 import { OrderBot } from 'src/model/OrderBot'
 
 @Injectable()
-export class TelegramService /*implements OnApplicationBootstrap*/ {
+export class TelegramService{
+    logger = new Logger(TelegramService.name)
     constructor(private orderBotService: OrderBotService) {}
 
     async webhook(body: any) {
@@ -12,7 +13,6 @@ export class TelegramService /*implements OnApplicationBootstrap*/ {
                 // (message) => this.processingMessage(message) is necessary for thier.orderBotService in this function
                 await Promise.all(body.messages.filter(m => m.id && m.text).map((message) => this.processingMessage(message)))
             case 'new_message':
-                console.log('body.message', body.message)
                 await this.processingMessage(body.message);
                 break
         }
@@ -24,6 +24,7 @@ export class TelegramService /*implements OnApplicationBootstrap*/ {
             message.text,
         )
         if (order) {
+            this.logger.log('new order', message.id, order.baseCoin)
             order.messageId = message.id
             await this.orderBotService.placeOrderBot(order)
         }
