633a09751a019090ea416af21f3fed20f5b09f37
Jimmy <tardyjim26@gmail.com>
2023-11-14 22:31:59 +0100

    update preferences default

--- src/model/User.ts
+++ src/model/User.ts
@@ -21,27 +21,31 @@ const defaultUserPreference: IUserPreferences = {
         levierSize: [
             {
                 minPrice: 0,
-                value: 10,
+                value: 5,
             }, // 0-1€, levier 10
             {
                 minPrice: 1,
-                value: 15,
+                value: 8,
             }, // 1-10€, levier 15
             {
                 minPrice: 10,
-                value: 20,
+                value: 12,
             }, // 10-50€, levier 20
             {
                 minPrice: 50,
-                value: 20,
+                value: 15,
             }, // 50-100, levier 20
             {
-                minPrice: 250,
-                value: 25,
+                minPrice: 100,
+                value: 17,
             }, // 250-1000, levier 25
             {
+                minPrice: 500,
+                value: 22,
+            },
+            {
                 minPrice: 1000,
-                value: 30,
+                value: 25,
             },
         ],
         marginCoin: 'USDT',
