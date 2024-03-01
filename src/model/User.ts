ad74e84a03e351f8369fa62b2fb2a3a558605bc1
Jimmy TARDY <jimmy.tardy@achatsolutions.fr>
2024-03-01 15:35:18 +0100

    correctif

--- src/model/User.ts
+++ src/model/User.ts
@@ -23,6 +23,7 @@ const defaultUserPreference: IUserPreferences = {
         marginCoin: 'USDT',
         baseCoinAuthorized: undefined,
         automaticUpdate: false,
+        pourcentage: 4
     },
 }
 
